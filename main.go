209e2c122425159b8089a0e68be5143ef303701b
Thomas Bennett <tbennett@talend.com>
2021-07-22 16:50:35 -0400

    updated with dbtcli.go skeleton

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -10,7 +11,14 @@ import (
 var e *echo.Echo
 
 func test(c echo.Context) error {
-	return c.String(http.StatusOK, "Wow this actually worked")
+	return c.String(http.StatusOK, "Wow this actually worked... new version!")
+}
+
+func GetProfiles(c echo.Context) error {
+	result := "{\"hello\":\"world\"}"
+	fmt.Println(c.Param("account_id"))
+	
+	return c.JSON(http.StatusOK, result)
 }
 
 func main() {
@@ -20,6 +28,7 @@ func main() {
 	e.Use(middleware.Recover())
 
 	e.GET("/test", test)
+	e.GET("/profiles/:account_id", GetProfiles)
 	fmt.Println("Listening for requests on port :8080")
 
 	e.Logger.Fatal(e.Start(":8080"))
